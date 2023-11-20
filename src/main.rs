bd9b90dcadea97a5fed685ba6a21ed51bf614917
Winter9998 <younesgt350@gmail.com>
2023-11-20 09:46:32 +0100

    new commit

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::io;
+use std::io::Write;
+
+
+pub fn password_maker() -> String {
+    let chars = "abcdefghijklmopqrstuvwxyz";
+    let upper_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let special = "1234567890@#&é'(§è!çà)-_{}";
+    let combined = format!("{}{}{}", chars, upper_chars, special);
+
+    print!("Choose a length: ");
+    io::stdout().flush().unwrap(); 
+
+    let mut choice = String::new();
+    io::stdin().read_line(&mut choice).unwrap();
+
+    let length: usize = choice.trim().parse().unwrap_or(8); // Default to 8 if parsing fails
+
+    let mut password = String::new();
+    for _ in 0..length {
+        let idx = rand::random::<usize>() % combined.len();
+        password.push(combined.chars().nth(idx).unwrap());
+    }
+
+    password
+}
+
+fn main() {
+    let password = password_maker();
+    println!("Generated password: {}", password);
+}
